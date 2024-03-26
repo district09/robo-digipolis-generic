12939946327db6f2e3c3b1fe13069e07440982e6
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2024-03-26 13:53:35 +0100

    Update to robo-digipolis-helpers 4.x

---
+++ src/Robo/Plugin/Commands/DigipolisGenericDeployCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace DigipolisGent\Robo\Generic\Robo\Plugin\Commands;
+
+use Consolidation\AnnotatedCommand\Events\CustomEventAwareInterface;
+use Robo\Tasks;
+
+class DigipolisGenericDeployCommand extends Tasks implements CustomEventAwareInterface
+{
+
+    use \DigipolisGent\Robo\Helpers\Traits\DigipolisHelpersDeployCommandUtilities;
+
+    /**
+     * Build a  site and push it to the servers.
+     *
+     * @param array $arguments
+     *   Variable amount of arguments. The last argument is the path to the
+     *   the private key file (ssh), the penultimate is the ssh user. All
+     *   arguments before that are server IP's to deploy to.
+     * @param array $opts
+     *   The options for this command.
+     *
+     * @option app The name of the app we're deploying. Used to determine the
+     *   directory to deploy to.
+     * @option worker The IP of the worker server. Defaults to the first server
+     *   given in the arguments.
+     *
+     * @usage --app=myapp 10.25.2.178 sshuser /home/myuser/.ssh/id_rsa
+     */
+    public function digipolisDeployGeneric(
+        array $arguments,
+        $opts = [
+            'app' => 'default',
+            'worker' => null,
+        ]
+    ) {
+        return $this->deploy($arguments, $opts);
+    }
+}
